cf6c8484405b6ebade16c028339b997c606dbcfe
xiapeimin <2536733090@qq.com>
2020-04-05 11:53:29 +0800

    v0.2

--- rectangle.js
+++ rectangle.js
@@ -1,19 +1,25 @@
+/*global $forkMeGH,$bszPageFooter*/
+/*eslint no-undef: "error"*/
+function i(e, t) {
+  return Math.round(e * Math.pow(10, t)) / Math.pow(10, t);
+}
+
 $(function() {
   var $width = $('#width'),
       $height = $('#height'),
       $btnCal = $('#calculate'),
       $perimeter = $('#perimeter'),
       $area = $('#area');
-  console.log($(window).height());
-  $forkMeGH.show("https://github.com/xiapeimin/rectangle");
+  //console.log($(window).height());
+  $forkMeGH.show('https://github.com/xiapeimin/rectangle');
   $bszPageFooter.show($('#times'));
 
   $btnCal.click(function(){
     var w = Number($width.val()),
         h = Number($height.val());
 
-    var p = 2 * (w + h),
-        a = w * h;
+    var p = i(2 * (w + h),2),
+        a = i(w * h,2);
 
     $perimeter.val(p);
     $area.val(a);
